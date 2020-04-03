5d0b8ca7ba31038576af515a23ff96450f198776
Gonçalo Saraiva <goncalosaraiva00@gmail.com>
2020-04-03 22:11:23 +0100

    Codigo correto, erro no botão

--- app/src/main/java/com/example/dados_pessoais/MostraActivity2.java
+++ app/src/main/java/com/example/dados_pessoais/MostraActivity2.java
@@ -2,7 +2,9 @@ package com.example.dados_pessoais;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MostraActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class MostraActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostra2);
+
+        Intent intent = getIntent();
+
+        String Nome = intent.getStringExtra(MainActivity.EXTRA_NOME);
+        String Telemovel = intent.getStringExtra(MainActivity.EXTRA_TELEMOVEL);
+        String Email = intent.getStringExtra(MainActivity.EXTRA_EMAIL);
+        String Idade = intent.getStringExtra(MainActivity.EXTRA_IDADE);
+        String Peso = intent.getStringExtra(MainActivity.EXTRA_PESO);
+        String Altura = intent.getStringExtra(MainActivity.EXTRA_ALTURA);
+
+        TextView textViewNome = findViewById(R.id.textViewNome);
+        TextView textViewTelemovel = findViewById(R.id.editTextTelemovel);
+        TextView textViewEmail = findViewById(R.id.editTextEmail);
+        TextView textViewIdade = findViewById(R.id.editTextIdade);
+        TextView textViewPeso = findViewById(R.id.editTextPeso);
+        TextView textViewAltura = findViewById(R.id.editTextAltura);
+
+        textViewNome.setText(Nome);
+        textViewTelemovel.setText(Telemovel);
+        textViewEmail.setText(Email);
+        textViewIdade.setText(Idade);
+        textViewPeso.setText(Peso);
+        textViewAltura.setText(Altura);
+
     }
 }
