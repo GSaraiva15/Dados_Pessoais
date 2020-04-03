52872a5d62c9c7c65ac99ecc236b2ac33880cb8e
Gonçalo Saraiva <goncalosaraiva00@gmail.com>
2020-04-03 18:22:24 +0100

    adicionada segunda atividade

--- app/src/main/java/com/example/dados_pessoais/MostraActivity2.java
+++ app/src/main/java/com/example/dados_pessoais/MostraActivity2.java
@@ -3,16 +3,12 @@ package com.example.dados_pessoais;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MostraActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    /*public void enviaMensagem (View view) {
-    }*/
+        setContentView(R.layout.activity_mostra2);
     }
 }
